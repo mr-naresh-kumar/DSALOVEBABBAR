363647b834fa5b3fae47e8d1507b619031ff18ed
$Naresh kumar <121456681+crazycodewithnaresh@users.noreply.github.com>
2024-03-09 20:51:45 +0530

    Add files via upload

---
+++ Week4Assignement/lck-diffpairarray.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#include<vector>
+// #include<sort>
+
+
+// leet code 532 
+
+using namespace std;
+
+int findPairs(vector<int>& nums, int k){
+    
+sort(nums.begin(), nums.end());
+set<pair<int, int>> ans;
+
+for (int i =0; i<nums.size(); i++){
+    // a[i];
+    // search-> a[i] + k [rightsubarray];
+}
+}
+
+int main(){
+
+
+
+
+}
