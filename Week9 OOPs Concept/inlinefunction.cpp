bca91fcc3a7db5ae764684ef938a2e373160f7a1
$Naresh kumar <121456681+narcoempw@users.noreply.github.com>
2024-05-14 22:23:15 +0530

    Add files via upload

---
+++ Week9 OOPs Concept/inlinefunction.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+
+  void numberShow(int num){
+    cout<< num<< endl;
+}
+
+int main(){
+
+    numberShow(10);
+    numberShow(10);
+    numberShow(10);
+    numberShow(10);
+    numberShow(10);
+    numberShow(10);
+    numberShow(10);
+    numberShow(10);
+    numberShow(10);
+    numberShow(10);
+    numberShow(10);
+    numberShow(10);
+
+    return 0;
+
+
+}
